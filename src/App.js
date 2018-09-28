861f0bc7e575d8f9ec751fe8f30e0a938d6f89bd
Alex Autem <autem.alex@gmail.com>
2018-09-28 16:21:38 -0500

    Add ExpansionSlots to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Clock from './components/Clock'
 import Content from './components/Content'
+import ExpansionSlot from './components/ExpansionSlot'
 import Footer from './components/Footer'
 import Greeting from './components/Greeting'
 import Header from './components/Header'
@@ -16,10 +17,14 @@ const App = ({ start }) => (
     </Header>
     <Content>
       <Panel numberOfColumns={3}>
-        <div className="Label Left">48</div>
+        {/* <div className="Label Left">48</div>
         <div className="Label Left">32</div>
         <div className="Label Left">16</div>
-        <div className="Label Left">0</div>
+        <div className="Label Left">0</div> */}
+
+        <ExpansionSlot />
+        <ExpansionSlot />
+        <ExpansionSlot />
       </Panel>
 
       <Panel numberOfColumns={1}>
@@ -27,10 +32,14 @@ const App = ({ start }) => (
       </Panel>
 
       <Panel numberOfColumns={3}>
-        <div className="Label Right">59</div>
+        {/* <div className="Label Right">59</div>
         <div className="Label Right">43</div>
         <div className="Label Right">27</div>
-        <div className="Label Right">11</div>
+        <div className="Label Right">11</div> */}
+
+        <ExpansionSlot />
+        <ExpansionSlot />
+        <ExpansionSlot />
       </Panel>
     </Content>
     <Footer />
