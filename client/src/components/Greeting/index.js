e748a902e0a8fbe994a4d5851bd37d0be9e2c709
Alex Autem <autem.alex@gmail.com>
2018-11-11 18:03:32 -0600

    Update Greeting component to use hooks

--- client/src/components/Greeting/index.js
+++ client/src/components/Greeting/index.js
@@ -1,18 +1,22 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import './Greeting.css'
 
 const propTypes = {
     //
 }
 
-const Greeting = () => (
-    <div className="Greeting">
-        <div className="Greeting_text">
-            Good Morning, Alex
+const Greeting = () => {
+    const [name, setName] = useState('Alex')
+
+    return (
+        <div className="Greeting">
+            <div className="Greeting_text">
+                Good Morning, {name}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 Greeting
 .propTypes = propTypes
