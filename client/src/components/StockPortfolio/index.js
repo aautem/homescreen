21414e458dc9691479b697624b0251a2f250d5e1
Alex Autem <autem.alex@gmail.com>
2018-11-13 20:20:19 -0600

    Update StockPortfolio with Stocks context

--- client/src/components/StockPortfolio/index.js
+++ client/src/components/StockPortfolio/index.js
@@ -1,137 +1,58 @@
-import axios from 'axios'
 import PropTypes from 'prop-types'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './StockPortfolio.css'
 
 import Stock from './Stock'
 
-const useStockPortfolio = () => {
-    const [stocks, setStocks] = useState([])
-
-    useEffect(() => {
-        const fetchStocks = (
-            setInterval(
-                () => {
-                    axios
-                    .get(
-                        'https://api.iextrading.com/1.0/stock/market/batch?symbols=btcusdt,ethusdt,ltcusdt,oprx&types=quote'
-                    )
-                    .then(({ data }) => {
-                        setStocks(
-                            Object
-                            .keys(data)
-                            .map(key => (
-                                data[key]
-                                .quote
-                            ))
-                        )
-                    })
-                },
-                60000,
-            )
-        )
-
-        return (
-            () => {
-                clearInterval(fetchStocks)
-            }
-        )
-    })
-
-    return stocks
-}
+import {
+    StocksInjector,
+    StocksProvider,
+} from '../../contexts/Stocks'
 
 const propTypes = {
-    //
+    // TODO
 }
 
-const StockPortfolio = () => {
-    const portfolio = useStockPortfolio()
+const StockPortfolio = () => (
+    <StocksProvider>
+        <StocksInjector>
+            {({ stocks }) => (
+                <div className="StockPortfolio">
+                    {
+                        stocks
+                        .map(stock => ({
+                            ...stock,
+                            symbol: (
+                                stock
+                                .symbol
+                                .includes('USDT')
+                                ? (
+                                    stock
+                                    .symbol
+                                    .replace(
+                                        'USDT',
+                                        '',
+                                    )
+                                )
+                                : (
+                                    stock
+                                    .symbol
+                                )
+                            ),
+                        }))
+                        .map(stock => (
+                            <Stock
+                                {...stock}
+                                key={stock.symbol}
+                            />
+                        ))
+                    }
+                </div>
+            )}
+        </StocksInjector>
+    </StocksProvider>
+)
 
-    return (
-        <div className="StockPortfolio">
-            {
-                portfolio
-                .map(stock => ({
-                    ...stock,
-                    symbol: (
-                        stock
-                        .symbol
-                        .includes('USDT')
-                        ? (
-                            stock
-                            .symbol
-                            .replace(
-                                'USDT',
-                                '',
-                            )
-                        )
-                        : (
-                            stock
-                            .symbol
-                        )
-                    ),
-                }))
-                .map(stock => (
-                    <Stock
-                        {...stock}
-                        key={stock.symbol}
-                    />
-                ))
-            }
-        </div>
-    )
-}
-
-StockPortfolio
-.propTypes = propTypes
+StockPortfolio.propTypes = propTypes
 
 export default StockPortfolio
-
-// https://api.iextrading.com/1.0/stock/BTCUSDT/quote
-// {
-//     "symbol":"BTCUSDT",
-//     "companyName":"Bitcoin USD",
-//     "primaryExchange":"crypto",
-//     "sector":"cryptocurrency",
-//     "calculationPrice":"realtime",
-//     "open":6460.01,
-//     "openTime":1542002292280,
-//     "close":6447.82641146,
-//     "closeTime":1542088692280,
-//     "high":6483.93,
-//     "low":6391.15,
-//     "latestPrice":6463.57,
-//     "latestSource":"Real time price",
-//     "latestTime":"12:58:12 AM",
-//     "latestUpdate":1542088692280,
-//     "latestVolume":10855.545845,
-//     "iexRealtimePrice":null,
-//     "iexRealtimeSize":null,
-//     "iexLastUpdated":null,
-//     "delayedPrice":null,
-//     "delayedPriceTime":null,
-//     "extendedPrice":null,
-//     "extendedChange":null,
-//     "extendedChangePercent":null,
-//     "extendedPriceTime":null,
-//     "previousClose":6460.01,
-//     "change":3.56,
-//     "changePercent":0.00055,
-//     "iexMarketPercent":null,
-//     "iexVolume":null,
-//     "avgTotalVolume":null,
-//     "iexBidPrice":null,
-//     "iexBidSize":null,
-//     "iexAskPrice":null,
-//     "iexAskSize":null,
-//     "marketCap":null,
-//     "peRatio":null,
-//     "week52High":null,
-//     "week52Low":null,
-//     "ytdChange":null,
-//     "bidPrice":6461.98,
-//     "bidSize":0.12377,
-//     "askPrice":6463.37,
-//     "askSize":0.044664
-// }
---
+++ client/src/contexts/Stocks.js
@@ -0,0 +1,111 @@
+import { get } from 'axios'
+import React, { Component, createContext } from 'react'
+
+// TODO: allow for custom symbols
+const apiAddress = 'https://api.iextrading.com/1.0/stock/market/batch?symbols=btcusdt,ethusdt,ltcusdt,oprx&types=quote'
+const fetchDelay = 30000
+
+export const StocksContext = createContext()
+
+export const StocksInjector = (
+  StocksContext.Consumer
+)
+
+export class StocksProvider extends Component {
+  constructor(props) {
+    super(props)
+    this.fetchStocksOnDelay = this.fetchStocksOnDelay.bind(this)
+    this.state = { stocks: [] }
+  }
+
+  componentDidMount() {
+    this.fetchStocksOnDelay(fetchDelay)
+  }
+
+  fetchStocksOnDelay(delay) {
+    get(apiAddress)
+    .then(({ data }) => {
+      const stocks = (
+        Object
+        .keys(data)
+        .map(key => data[key])
+        .map(({ quote }) => quote)
+      )
+
+      this
+      .setState({
+        stocks,
+      })
+    })
+
+    // setTimeout(
+    //   this.fetchStocksOnDelay,
+    //   delay,
+    // )
+  }
+
+  render() {
+    console.log({ state: this.state })
+
+    return (
+      <StocksContext.Provider
+        value={this.state}
+      >
+        {
+            this
+            .props
+            .children
+        }
+      </StocksContext.Provider>
+    )
+  }
+}
+
+// https://api.iextrading.com/1.0/stock/BTCUSDT/quote
+//
+// {
+//     "symbol":"BTCUSDT",
+//     "companyName":"Bitcoin USD",
+//     "primaryExchange":"crypto",
+//     "sector":"cryptocurrency",
+//     "calculationPrice":"realtime",
+//     "open":6460.01,
+//     "openTime":1542002292280,
+//     "close":6447.82641146,
+//     "closeTime":1542088692280,
+//     "high":6483.93,
+//     "low":6391.15,
+//     "latestPrice":6463.57,
+//     "latestSource":"Real time price",
+//     "latestTime":"12:58:12 AM",
+//     "latestUpdate":1542088692280,
+//     "latestVolume":10855.545845,
+//     "iexRealtimePrice":null,
+//     "iexRealtimeSize":null,
+//     "iexLastUpdated":null,
+//     "delayedPrice":null,
+//     "delayedPriceTime":null,
+//     "extendedPrice":null,
+//     "extendedChange":null,
+//     "extendedChangePercent":null,
+//     "extendedPriceTime":null,
+//     "previousClose":6460.01,
+//     "change":3.56,
+//     "changePercent":0.00055,
+//     "iexMarketPercent":null,
+//     "iexVolume":null,
+//     "avgTotalVolume":null,
+//     "iexBidPrice":null,
+//     "iexBidSize":null,
+//     "iexAskPrice":null,
+//     "iexAskSize":null,
+//     "marketCap":null,
+//     "peRatio":null,
+//     "week52High":null,
+//     "week52Low":null,
+//     "ytdChange":null,
+//     "bidPrice":6461.98,
+//     "bidSize":0.12377,
+//     "askPrice":6463.37,
+//     "askSize":0.044664
+// }
