5b42cd398a2a9924ee4a86a607687ce9a488927b
Alex Autem <autem.alex@gmail.com>
2018-11-17 02:01:38 -0600

    Add template for calendar api response

---
+++ client/src/contexts/Calendar.js
@@ -0,0 +1,51 @@
+//   summary: 'Grocery Shopping',         
+//   start: { date: '2018-11-18' },       
+//   end: { date: '2018-11-19' },         
+// //   start:                             
+// //    { dateTime: '2018-11-19T09:45:00-0
+// //      timeZone: 'America/Chicago' },  
+// //   end:                               
+// //    { dateTime: '2018-11-19T10:00:00-0
+// //      timeZone: 'America/Chicago' },  
+
+
+
+
+
+
+
+//   kind: 'calendar#event',              
+//   etag: '"3077337348791000"',          
+//   id: '05j35smnonuj17767rb794l0ea_20181
+//   status: 'confirmed',                 
+//   htmlLink: 'https://www.google.com/cal
+//   leEBt',                                   
+//   created: '2018-09-21T16:35:51.000Z', 
+//   updated: '2018-10-17T15:19:56.803Z', 
+//   summary: 'Grocery Shopping',         
+//   creator:                             
+//     { email: 'autem.alex@gmail.com',    
+//       displayName: 'Alex Autem',        
+//       self: true },                     
+//   organizer:                           
+//     { email: 'autem.alex@gmail.com',    
+//       displayName: 'Alex Autem',        
+//       self: true },                     
+
+//   start: { date: '2018-11-18' },       
+//   end: { date: '2018-11-19' },         
+// //   start:                             
+// //    { dateTime: '2018-11-19T09:45:00-0
+// //      timeZone: 'America/Chicago' },  
+// //   end:                               
+// //    { dateTime: '2018-11-19T10:00:00-0
+// //      timeZone: 'America/Chicago' },  
+
+//   recurringEventId: '05j35smnonuj17767r
+//   originalStartTime: { date: '2018-11-1
+//   iCalUID: '05j35smnonuj17767rb794l0ea@
+//   sequence: 1,                         
+//   extendedProperties: { private: [Objec
+//   guestsCanInviteOthers: false,        
+//   guestsCanSeeOtherGuests: false,      
+//   reminders: { useDefault: false } }
