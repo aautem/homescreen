d2c4167b177f75c329bb08dc5dac00fbf66da835
Alex Autem <autem.alex @gmail.com>
2021-08-08 23:48:38 -0500

    Add mock data for weather display

--- client/src/components/Today/CurrentWeather.js
+++ client/src/components/Today/CurrentWeather.js
@@ -1,111 +1,106 @@
-import getTemperature from '../../utils/getTemperature'
+import getTemperature from "../../utils/getTemperature";
 // import PropTypes from 'prop-types'
-import React from 'react'
-import renderWeatherIcon from '../../utils/renderWeatherIcon'
-import './CurrentWeather.css'
+import React from "react";
+import renderWeatherIcon from "../../utils/renderWeatherIcon";
+import "./CurrentWeather.css";
 
-import { WeatherInjector } from '../../contexts/Weather'
+import { WeatherInjector } from "../../contexts/Weather";
 
-// currently
-  // apparentTemperature
-  // icon
-  // precipProbability
-  // summary
-  // temperature
-  // windSpeed
+const mockData = {
+  alerts: [{ title: "Testing the alerts" }],
+  currently: {
+    // apparentTemperature: "",
+    icon: "clear-night",
+    // precipProbability: "",
+    summary: "Slightly cloudy skies with a chance of rain",
+    temperature: 84,
+    // windSpeed: "",
+  },
+  daily: {
+    data: [
+      {
+        precipProbability: 12,
+        precipType: "rain",
+        temperatureHigh: 101,
+        temperatureLow: 62,
+      },
+    ],
+  },
+  hourly: {},
+  isLoading: false,
+};
 
 // daily
-  // icon
-  // summary
-  // data
-    // icon
-    // moonPhase
-    // precipProbability
-    // precipType
-    // summary
-    // sunriseTime
-    // sunsetTime
-    // temeratureHigh
-    // temperatureLow
-    // windSpeed
+// icon
+// summary
+// data
+// icon
+// moonPhase
+// precipProbability
+// precipType
+// summary
+// sunriseTime
+// sunsetTime
+// temeratureHigh
+// temperatureLow
+// windSpeed
 
 // hourly
-  // icon
-  // summary
-  // data
-    // icon
-    // precipProbability
-    // summary
-    // temerature
-    // time
-    // windSpeed
+// icon
+// summary
+// data
+// icon
+// precipProbability
+// summary
+// temerature
+// time
+// windSpeed
 
 const CurrentWeather = () => (
   <WeatherInjector>
-    {({ alerts, currently, daily, isLoading }) => (
+    {({ alerts, currently, daily, isLoading }) =>
       // console.log({ alerts, currently, daily, isLoading }) ||
 
-      isLoading
-      ? null // TODO: Replace with `LoadingIndicator`
-      : (
+      mockData.isLoading ? null : ( // TODO: Replace with `LoadingIndicator`
         <div className="CurrentWeather">
           <div className="CurrentWeather_data">
             <div className="CurrentWeather_temperature">
-              {
-                getTemperature(
-                  currently
-                )
-              }
-            </div>
-    
-            <div className="CurrentWeather_outlook">
-              <div className="CurrentWeather_extremeTemperatures">
-                {Math.round(daily.data[0].temperatureLow || 0)}°F / {Math.round(daily.data[0].temperatureHigh || 0)}°F
-              </div>
-              <div className="CurrentWeather_precipitation">
-                {
-                  (
-                    (
-                      daily
-                      .data[0]
-                      .precipProbability
-                      || 0
-                    )
-                    * 100
-                  )
-                  .toFixed(0)
-                }
-                % chance of {daily.data[0].precipType}
-              </div>
-            </div>
-          </div>
-    
-          <div className="CurrentWeather_description">
-            <div className="CurrentWeather_summary">
-              {currently.summary}
+              {getTemperature(mockData.currently)}
             </div>
 
-            {
-              alerts
-              && (
-                <div className="CurrentWeather_alert">
-                  {alerts[0].title}
-                </div>
-              )
-            }
+            <div className="CurrentWeather_outlook">
+              <div className="CurrentWeather_extremeTemperatures">
+                {Math.round(mockData.daily.data[0].temperatureLow || 0)}°F /{" "}
+                {Math.round(mockData.daily.data[0].temperatureHigh || 0)}°F
+              </div>
+              <div className="CurrentWeather_precipitation">
+                {(
+                  (mockData.daily.data[0].precipProbability || 0) * 100
+                ).toFixed(0)}
+                % chance of {mockData.daily.data[0].precipType}
+              </div>
+            </div>
           </div>
-    
+
+          <div className="CurrentWeather_description">
+            <div className="CurrentWeather_summary">
+              {mockData.currently.summary}
+            </div>
+
+            {mockData.alerts && (
+              <div className="CurrentWeather_alert">
+                {mockData.alerts[0].title}
+              </div>
+            )}
+          </div>
+
           <div className="CurrentWeather_icon">
-            {
-              renderWeatherIcon(
-                currently
-              )
-            }
+            {renderWeatherIcon(mockData.currently)}
           </div>
         </div>
       )
-    )}
+    }
   </WeatherInjector>
-)
+);
 
-export default CurrentWeather
+export default CurrentWeather;
--- client/src/contexts/Weather.js
+++ client/src/contexts/Weather.js
@@ -1,66 +1,56 @@
-import axios from 'axios'
-import React, { Component, createContext } from 'react'
+import axios from "axios";
+import React, { Component, createContext } from "react";
 
-const apiAddress = 'http://localhost:3000/api/weather'
-const fetchDelay = 300000
-const WeatherContext = createContext()
+const apiAddress = "http://localhost:3000/api/weather";
+const fetchDelay = 300000;
+const WeatherContext = createContext();
 
-export const WeatherInjector = (
-  WeatherContext.Consumer
-)
+export const WeatherInjector = WeatherContext.Consumer;
 
 export class WeatherProvider extends Component {
   constructor(props) {
-    super(props)
-    this.fetchWeatherOnDelay = this.fetchWeatherOnDelay.bind(this)
+    super(props);
+    this.fetchWeatherOnDelay = this.fetchWeatherOnDelay.bind(this);
     this.state = {
       alerts: null,
       currently: {},
       daily: {},
       hourly: {},
       isLoading: true,
-    }
+    };
   }
 
   componentDidMount() {
-    this.fetchWeatherOnDelay()
+    // this.fetchWeatherOnDelay()
   }
 
   fetchWeatherOnDelay() {
-    this.setState({
-      isLoading: true,
-    })
-
-    axios
-    .get(apiAddress)
-    .then(({ data }) => {
-      this.setState({
-        alerts: data.alerts,
-        currently: data.currently,
-        daily: data.daily,
-        hourly: data.hourly,
-        isLoading: false,
-      })
-    })
-    .catch(console.error)
-
-    setTimeout(
-      this.fetchWeatherOnDelay,
-      fetchDelay,
-    )
+    // this.setState({
+    //   isLoading: true,
+    // })
+    // axios
+    // .get(apiAddress)
+    // .then(({ data }) => {
+    //   this.setState({
+    //     alerts: data.alerts,
+    //     currently: data.currently,
+    //     daily: data.daily,
+    //     hourly: data.hourly,
+    //     isLoading: false,
+    //   })
+    // })
+    // .catch(console.error)
+    // setTimeout(
+    //   this.fetchWeatherOnDelay,
+    //   fetchDelay,
+    // )
   }
 
   render() {
     return (
-      <WeatherContext.Provider
-        value={this.state}
-      >
-        {
-          this
-          .props
-          .children
-        }
+      <WeatherContext.Provider value={this.state}>
+        {this.props.children}
       </WeatherContext.Provider>
-    )
+    );
   }
 }
