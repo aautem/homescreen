eec4cd6badc10066f6244ae4c04fca888411aa30
Alex Autem <autem.alex @gmail.com>
2021-08-08 23:57:40 -0500

    Add mock data for calendar display

--- client/src/components/calendar/index.js
+++ client/src/components/calendar/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import './Calendar.css'
+import React from "react";
+import "./Calendar.css";
 
-import Event from './Event'
+import Event from "./Event";
 
-import {
-  CalendarInjector,
-  CalendarProvider,
-} from '../../contexts/Calendar'
+import { CalendarInjector, CalendarProvider } from "../../contexts/Calendar";
+
+const mockData = {
+  isLoading: false,
+  events: [
+    {
+      id: "00sma1pl43r9kalima4hpuriqd_20181121T133000Z",
+      colorId: "9",
+      description:
+        "Surprise party, please do not ruin it for anyone, especially mike.",
+      location: "New York, NY",
+      start: { date: "2021-07-12" },
+      summary: "Mike Birthday Party",
+    },
+    {
+      id: "00sma1pl43r9kalima4hpuriqd_20181121T133000Z",
+      colorId: "9",
+      description:
+        "Surprise party, please do not ruin it for anyone, especially mike.",
+      location: "New York, NY",
+      start: { date: "2021-07-12" },
+      summary: "Mike Birthday Party",
+    },
+    {
+      id: "00sma1pl43r9kalima4hpuriqd_20181121T133000Z",
+      colorId: "9",
+      description:
+        "Surprise party, please do not ruin it for anyone, especially mike.",
+      location: "New York, NY",
+      start: { date: "2021-07-12" },
+      summary: "Mike Birthday Party",
+    },
+  ],
+};
 
 const Calendar = () => (
   <CalendarProvider>
@@ -14,24 +44,18 @@ const Calendar = () => (
       <div className="Calendar_content">
         <div className="Calendar_events">
           <CalendarInjector>
-            {({ events }) => (
-              events
-              .map(({ id, ...event }) => (
-                <Event
-                  key={id}
-                  {...event}
-                />
+            {({ events }) =>
+              mockData.events.map(({ id, ...event }) => (
+                <Event key={id} {...event} />
               ))
-            )}
+            }
           </CalendarInjector>
         </div>
 
-        <div className="Calendar_happeningSoon">
-          Happening Soon
-        </div>
+        <div className="Calendar_happeningSoon">Happening Soon</div>
       </div>
     </div>
   </CalendarProvider>
-)
+);
 
-export default Calendar
+export default Calendar;
--- client/src/contexts/Calendar.js
+++ client/src/contexts/Calendar.js
@@ -6,102 +6,92 @@
 // start: { date: '2018-11-18' },
 // summary: 'Grocery Shopping',
 
-import axios from 'axios'
-import React, { Component, createContext } from 'react'
+import axios from "axios";
+import React, { Component, createContext } from "react";
 
-const apiAddress = 'http://localhost:3000/api/calendar'
-const fetchDelay = 300000
-const CalendarContext = createContext()
+const apiAddress = "http://localhost:3000/api/calendar";
+const fetchDelay = 300000;
+const CalendarContext = createContext();
 
-export const CalendarInjector = (
-  CalendarContext.Consumer
-)
+export const CalendarInjector = CalendarContext.Consumer;
 
 export class CalendarProvider extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       events: [],
       isLoading: true,
-    }
-    this.fetchCalendarOnDelay = this.fetchCalendarOnDelay.bind(this)
+    };
+    this.fetchCalendarOnDelay = this.fetchCalendarOnDelay.bind(this);
   }
 
   componentDidMount() {
-    this.fetchCalendarOnDelay()
+    // this.fetchCalendarOnDelay()
   }
 
   fetchCalendarOnDelay() {
-    this.setState({
-      isLoading: true,
-    })
-
-    axios
-    .get(apiAddress)
-    .then(({ data }) => {
-      this.setState({
-        events: data,
-        isLoading: false,
-      })
-    })
-    .catch(console.error)
-
-    setTimeout(
-      this.fetchCalendarOnDelay,
-      fetchDelay,
-    )
+    // this.setState({
+    //   isLoading: true,
+    // })
+    // axios
+    // .get(apiAddress)
+    // .then(({ data }) => {
+    //   this.setState({
+    //     events: data,
+    //     isLoading: false,
+    //   })
+    // })
+    // .catch(console.error)
+    // setTimeout(
+    //   this.fetchCalendarOnDelay,
+    //   fetchDelay,
+    // )
   }
 
   render() {
     return (
-      <CalendarContext.Provider
-        value={this.state}
-      >
-        {
-          this
-          .props
-          .children
-        }
+      <CalendarContext.Provider value={this.state}>
+        {this.props.children}
       </CalendarContext.Provider>
-    )
+    );
   }
 }
 
 // colorId: '9'
 // description: 'this is a description',
 // location: 'CP Conference Room',
-//   kind: 'calendar#event',              
-//   etag: '"3077337348791000"',          
+//   kind: 'calendar#event',
+//   etag: '"3077337348791000"',
 //   id: '05j35smnonuj17767rb794l0ea_20181
-//   status: 'confirmed',                 
+//   status: 'confirmed',
 //   htmlLink: 'https://www.google.com/cal
-//   leEBt',                                   
-//   created: '2018-09-21T16:35:51.000Z', 
-//   updated: '2018-10-17T15:19:56.803Z', 
-//   summary: 'Grocery Shopping',         
-//   creator:                             
-//     { email: 'autem.alex@gmail.com',    
-//       displayName: 'Alex Autem',        
-//       self: true },                     
-//   organizer:                           
-//     { email: 'autem.alex@gmail.com',    
-//       displayName: 'Alex Autem',        
-//       self: true },                     
+//   leEBt',
+//   created: '2018-09-21T16:35:51.000Z',
+//   updated: '2018-10-17T15:19:56.803Z',
+//   summary: 'Grocery Shopping',
+//   creator:
+//     { email: 'autem.alex@gmail.com',
+//       displayName: 'Alex Autem',
+//       self: true },
+//   organizer:
+//     { email: 'autem.alex@gmail.com',
+//       displayName: 'Alex Autem',
+//       self: true },
 
-//   start: { date: '2018-11-18' },       
-//   end: { date: '2018-11-19' },         
-// //   start:                             
+//   start: { date: '2018-11-18' },
+//   end: { date: '2018-11-19' },
+// //   start:
 // //    { dateTime: '2018-11-19T09:45:00-0
-// //      timeZone: 'America/Chicago' },  
-// //   end:                               
+// //      timeZone: 'America/Chicago' },
+// //   end:
 // //    { dateTime: '2018-11-19T10:00:00-0
-// //      timeZone: 'America/Chicago' },  
+// //      timeZone: 'America/Chicago' },
 
 //   recurringEventId: '05j35smnonuj17767r
 //   originalStartTime: { date: '2018-11-1
 //   iCalUID: '05j35smnonuj17767rb794l0ea@
-//   sequence: 1,                         
+//   sequence: 1,
 //   extendedProperties: { private: [Objec
-//   guestsCanInviteOthers: false,        
-//   guestsCanSeeOtherGuests: false,      
+//   guestsCanInviteOthers: false,
+//   guestsCanSeeOtherGuests: false,
 //   reminders: { useDefault: false } }
