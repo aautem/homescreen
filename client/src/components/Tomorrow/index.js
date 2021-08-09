0c558b58ae3caa16b27bb22a1974fe36340b706a
Alex Autem <autem.alex @gmail.com>
2021-08-09 00:12:31 -0500

    Add mock data for tomorrow display

--- client/src/components/Tomorrow/index.js
+++ client/src/components/Tomorrow/index.js
@@ -1,47 +1,53 @@
-import React from 'react'
-import renderWeatherIcon from '../../utils/renderWeatherIcon'
-import './Tomorrow.css'
+import React from "react";
+import renderWeatherIcon from "../../utils/renderWeatherIcon";
+import "./Tomorrow.css";
 
 // import { FiChevronsLeft, FiChevronsRight } from 'react-icons/fi'
-import { WeatherInjector } from '../../contexts/Weather'
+import { WeatherInjector } from "../../contexts/Weather";
+
+const mockData = {
+  daily: {
+    icon: "clear-day",
+    summary: "Very sunny, get ya sunscreen!",
+  },
+  hourly: {
+    icon: "clear-day",
+    summary: "Very sunny, get ya sunscreen!",
+  },
+  isLoading: false,
+};
 
 const Tomorrow = () => (
   <WeatherInjector>
-    {({ daily, hourly, isLoading }) => (
-      console.log({ daily, hourly }) ||
+    {({ daily, hourly, isLoading }) =>
+      console.log({ daily, hourly }) || (
+        <div className="Tomorrow">
+          <div className="Tomorrow_content">
+            <div className="Tomorrow_temperature">
+              {!mockData.isLoading && renderWeatherIcon(mockData.hourly)}
+            </div>
+            <div className="Tomorrow_labels">
+              <div className="Tomorrow_topLabel">
+                {mockData.hourly.summary}
 
-      <div className="Tomorrow">
-        <div className="Tomorrow_content">
-          <div className="Tomorrow_temperature">
-            {
-              !isLoading
-              && renderWeatherIcon(hourly)
-            }
-          </div>
-          <div className="Tomorrow_labels">
-            <div className="Tomorrow_topLabel">
-              {hourly.summary}
-
-              {/* <FiChevronsLeft />
+                {/* <FiChevronsLeft />
               Tonight */}
-            </div>
-            <div className="Tomorrow_bottomLabel">
-              {daily.summary}
+              </div>
+              <div className="Tomorrow_bottomLabel">
+                {mockData.daily.summary}
 
-              {/* Tomorrow
+                {/* Tomorrow
               <FiChevronsRight /> */}
+              </div>
             </div>
-          </div>
-          <div className="Tomorrow_icon">
-            {
-              !isLoading
-              && renderWeatherIcon(daily)
-            }
+            <div className="Tomorrow_icon">
+              {!mockData.isLoading && renderWeatherIcon(mockData.daily)}
+            </div>
           </div>
         </div>
-      </div>
-    )}
+      )
+    }
   </WeatherInjector>
-)
+);
 
-export default Tomorrow
+export default Tomorrow;
