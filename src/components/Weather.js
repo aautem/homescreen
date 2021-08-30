505c98285f343fa939e4f3ae2c235439359714c8
Alex Autem <autem.alex @gmail.com>
2021-08-29 22:30:09 -0500

    Update weather icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
 import Box from '@material-ui/core/Box'
+import Brightness2OutlinedIcon from '@material-ui/icons/Brightness2Outlined'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import CloseIcon from '@material-ui/icons/Close'
 import CloudOutlinedIcon from '@material-ui/icons/CloudOutlined'
+import FlashOnOutlinedIcon from '@material-ui/icons/FlashOnOutlined'
+import GrainOutlinedIcon from '@material-ui/icons/GrainOutlined'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
@@ -10,9 +14,26 @@ import WbSunnyOutlinedIcon from '@material-ui/icons/WbSunnyOutlined'
 
 import { useWeather } from '../hooks/useWeather'
 
+// https://openweathermap.org/weather-conditions#How-to-get-icon-URL
 const icons = {
-  '01d': WbSunnyOutlinedIcon,
-  '03d': CloudOutlinedIcon,
+  '01d': WbSunnyOutlinedIcon, // clear sky, day
+  '01n': Brightness2OutlinedIcon, // clear sky, night
+  '02d': CloudOutlinedIcon, // few clouds, day
+  '02n': CloudOutlinedIcon, // few clouds, night
+  '03d': CloudOutlinedIcon, // scattered clouds, day
+  '03n': CloudOutlinedIcon, // scattered clouds, night
+  '04d': CloudOutlinedIcon, // broken clouds, day
+  '04n': CloudOutlinedIcon, // broken clouds, night
+  '09d': GrainOutlinedIcon, // shower rain, day
+  '09n': GrainOutlinedIcon, // shower rain, night
+  '10d': GrainOutlinedIcon, // rain, day
+  '10n': GrainOutlinedIcon, // rain, night
+  '11d': FlashOnOutlinedIcon, // thunderstorm, day
+  '11n': FlashOnOutlinedIcon, // thunderstorm, night
+  '13d': AcUnitOutlinedIcon, // snow, day
+  '13n': AcUnitOutlinedIcon, // snow, night
+  '50d': GrainOutlinedIcon, // mist, day
+  '50n': GrainOutlinedIcon, // mist, night
 }
 
 const useStyles = makeStyles(theme => ({
