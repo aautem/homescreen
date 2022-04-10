ba3a71016cfc649a038e0e4084374a643124bbe6
Alex Autem <autem.alex @gmail.com>
2022-04-10 12:55:14 -0500

    Round rain chance

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -111,7 +111,7 @@ const Forecast = () => {
                 <div>{w.time}</div>
                 {Icon ? <Icon size="2.25rem" /> : <div>?</div>}
                 <div>{Math.round(w.temp)}&deg;</div>
-                <div>{w.pop}%</div>
+                <div>{Math.round(w.pop * 100)}%</div>
               </Fragment>
             )
           })}
@@ -124,7 +124,7 @@ const Forecast = () => {
                 <div>{w.day}</div>
                 {Icon ? <Icon size="2.25rem" /> : <div>?</div>}
                 <div>{Math.round(w.temp.day)}&deg;</div>
-                <div>{w.pop * 100}%</div>
+                <div>{Math.round(w.pop * 100)}%</div>
               </Fragment>
             )
           })}
